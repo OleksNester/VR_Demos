31d2413e1f6510e0d9be5fee4d147dc80cc41a4a
day <matilda.tereshkova@gmail.com>
2016-03-12 02:59:08 +0200

    Added button event: camera-flythrough

---
+++ RealEstate/Assets/SplineController/SplineControllerMy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplineControllerMy : SplineController {
+	
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Go() {
+		FollowSpline();
+	}
+}
