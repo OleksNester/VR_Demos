c8b385f7e0a770b9b0413dea8511012eefa30380
day <matilda.tereshkova@gmail.com>
2016-03-15 19:40:54 +0200

    Added button.onClick event listener which loads "day" scene (sketch so far. todo: check load/unload details)

---
+++ RealEstate/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LoadScene(string sceneName) {
+		SceneManager.LoadScene (sceneName);
+		Scene scene = SceneManager.GetSceneByName(sceneName);
+		SceneManager.SetActiveScene(scene);
+	}
+}
