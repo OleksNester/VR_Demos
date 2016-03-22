31260441868388faa441da374e6d33f680542729
day <matilda.tereshkova@gmail.com>
2016-03-22 14:08:17 +0200

    Added Android Back nav. button to Night scene and Video360

---
+++ HazelwoodLoftDemo/Assets/Scripts/BackButtonListener.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class BackButtonListener : MonoBehaviour {
+
+	public string BackToScene;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool back = Input.GetKeyDown(KeyCode.Escape);
+
+		if(back)
+		{
+			Scene oldScene = SceneManager.GetActiveScene ();
+			SceneManager.LoadScene(BackToScene);
+			Scene newScene = SceneManager.GetSceneByName(BackToScene);
+			SceneManager.SetActiveScene(newScene);
+			SceneManager.UnloadScene(oldScene.buildIndex);
+		}
+	}
+
+}
