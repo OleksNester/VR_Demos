cd6ed3f982b2630398b43ddccdecdbf27b48e1d9
day <matilda.tereshkova@gmail.com>
2016-03-23 21:26:48 +0200

    Changed navigation in Night scene: points are on the floor now; changed scripts also.

---
+++ HazelwoodLoftDemo/Assets/Scripts/NavigationController2.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavigationController2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (AutoStart) {
+			MoveTowards (MovemmediatelyTo);
+		}
+	}
+
+	public bool AutoStart;
+	public Transform MovemmediatelyTo;
+
+	public float Speed;
+	private Transform target;
+	private bool start = false;
+
+	public void MoveTowards(Transform t) {
+		target = t;
+		start = true;
+	}
+
+	void Update() {
+		if (start) {
+			float step = Speed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+			if (transform.position.Equals (target.position)) {
+				start = false;
+			}
+		}
+	}
+
+	/*
+public Transform target;
+    public float speed;
+    void Update() {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+	*/
+}
