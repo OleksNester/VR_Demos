703c129006131d3542ddeb3a87122c05b8eff6cb
day <matilda.tereshkova@gmail.com>
2016-03-23 22:14:15 +0200

    Added some Scripts and graphics to lib/

---
+++ lib/Scripts/StartMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LoadScene(string sceneName) {
+		Scene oldScene = SceneManager.GetActiveScene ();
+		SceneManager.LoadScene(sceneName);
+		Scene newScene = SceneManager.GetSceneByName(sceneName);
+		SceneManager.SetActiveScene(newScene);
+		SceneManager.UnloadScene(oldScene.buildIndex);
+	}
+}
